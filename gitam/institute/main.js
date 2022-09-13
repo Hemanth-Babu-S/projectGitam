e667ea910566d62f16cfc947511aed5ac77c8ace
Lenovo <hemanth.s.2018@gmail.com>
2022-09-13 17:01:28 +0530

    first commit

---
+++ gitam/institute/main.js
@@ -0,0 +1,108 @@
+function load(num)
+{
+	switch(num)
+	{
+		case 1:
+			document.getElementById("imageone").style.display='block';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 2:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='block';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 3:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='block';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 4:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='block';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 5:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='block';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 6:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='block';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 7:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='block';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 8:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='block';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 9:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='block';
+			break;
+		default:
+			break;
+	}
+	return;
+}
---
+++ gitam/placements/main.js
@@ -0,0 +1,88 @@
+function load(num)
+{
+	switch(num)
+	{
+		case 1:
+			document.getElementById("imageone").style.display='block';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 2:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='block';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 3:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='block';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 4:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='block';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 5:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='block';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 6:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='block';
+			document.getElementById("imageseven").style.display='none';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		case 7:
+			document.getElementById("imageone").style.display='none';
+			document.getElementById("imagetwo").style.display='none';
+			document.getElementById("imagethree").style.display='none';
+			document.getElementById("imagefour").style.display='none';
+			document.getElementById("imagefive").style.display='none';
+			document.getElementById("imagesix").style.display='none';
+			document.getElementById("imageseven").style.display='block';
+			document.getElementById("imageeight").style.display='none';
+			document.getElementById("archives").style.display='none';
+			break;
+		
+		
+		default:
+			break;
+	}
+	return;
+}
